242081f9c052c66c63f8b4594675efdad703c74e
2dukes <ruipinto02@hotmail.com>
2022-01-05 15:04:24 +0000

    Update: connection function to identify non-online peers.

--- src/utils.py
+++ src/utils.py
@@ -9,19 +9,23 @@ import time
 
 
 async def make_connection(host, port, msg_content):
-    reader, writer = await asyncio.open_connection(
-        host, port)
+    try:
+        reader, writer = await asyncio.open_connection(
+            host, port)
 
-    print_log(msg_content)
-    writer.write(json.dumps(msg_content).encode())
-    writer.write_eof()
+        print_log(msg_content)
+        writer.write(json.dumps(msg_content).encode())
+        writer.write_eof()
 
-    data = await reader.read()
-    response = json.loads(data.decode())
+        data = await reader.read()
+        response = json.loads(data.decode())
 
-    writer.close()
-
-    return response
+        writer.close()
+        return response
+        
+    except Exception as err:
+        print_log(str(err))
+        return None
 
 def get_time():
     try:
